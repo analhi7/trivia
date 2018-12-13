31b037545aa796c4390856726e0f59a4c7dfec92
analhi7 <analhi.e.m7@gmail.com>
2018-12-13 09:23:39 -0600

    modificando liga del index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,5 +2,5 @@ function usuario(){
 let valor = document.getElementById("nombre").value;
 localStorage.setItem("nombreUsuario", valor);
 //alert(localStorage.getItem("nombreUsuario"));
-location.href = "C:/Users/Sandy/Desktop/Nuevo_Proyecto/bienvenida.html"; 
+location.href = "./bienvenida.html"; 
 }
